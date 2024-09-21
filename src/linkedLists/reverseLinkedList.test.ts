d287bf02116913abb0a48ad5911c53ef5c53c34f
bloweyelashwish <lisaxbarkar@gmail.com>
2024-09-22 00:34:42 +0300

    unit(linked-lists): add reverseLinkedList test

--- src/linkedLists/reverseLinkedList.test.ts
+++ src/linkedLists/reverseLinkedList.test.ts
@@ -2,7 +2,27 @@
 
 import { test, expect } from 'vitest';
 import { reverseList } from './reverseLinkedList';
+import { ListNode } from 'src/utils/singlyLinkedList/ListNode';
+
+function createLinkedList(input: number[]) {
+  const head = new ListNode(input[0]);
+  let current = head;
+
+  for (let i = 1; i < input.length; i++) {
+    current.next = new ListNode(input[i]);
+    current = current.next;
+  }
+
+  return head;
+}
 
 test('reverseLinkedList', () => {
+  const arr = [1, 2, 3, 4, 5];
+  const reverseArr = [...arr].reverse();
 
-})
+  const testList = createLinkedList(arr);
+  const expectedList = createLinkedList(reverseArr);
+
+  const result = reverseList(testList);
+  expect(result).toEqual(expectedList);
+});
